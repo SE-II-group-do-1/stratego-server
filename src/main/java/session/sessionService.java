9025a33bd7f56bad7d028188ecfa30a7cf863f82
klaus-maria <63562466+klaus-maria@users.noreply.github.com>
2024-03-27 09:55:24 +0100

    fix: forgot to make session class impl methods

--- src/main/java/session/sessionService.java
+++ src/main/java/session/sessionService.java
@@ -1,4 +1,7 @@
 package session;
 
 public class sessionService implements sessionServiceI {
+    public void createSession(){};
+    public void addToSession(){};
+    public void closeSession(){};
 }
