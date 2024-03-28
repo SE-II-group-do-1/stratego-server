27eef93a031feb49c5c3ffed4c2ef3b7c9b9809b
klaus-maria <63562466+klaus-maria@users.noreply.github.com>
2024-03-28 19:07:38 +0100

    fix: gave Lake pieces lake rank

--- src/main/java/session/Board.java
+++ src/main/java/session/Board.java
@@ -6,15 +6,15 @@ public class Board {
     public Board(){
         this.fields =  new Piece[10][10];
         // set lakes
-        fields[4][2] = new Piece();
-        fields[4][3] = new Piece();
-        fields[5][2] = new Piece();
-        fields[5][3] = new Piece();
+        fields[4][2] = new Piece(Rank.LAKE);
+        fields[4][3] = new Piece(Rank.LAKE);
+        fields[5][2] = new Piece(Rank.LAKE);
+        fields[5][3] = new Piece(Rank.LAKE);
 
-        fields[4][6] = new Piece();
-        fields[4][7] = new Piece();
-        fields[5][6] = new Piece();
-        fields[5][7] = new Piece();
+        fields[4][6] = new Piece(Rank.LAKE);
+        fields[4][7] = new Piece(Rank.LAKE);
+        fields[5][6] = new Piece(Rank.LAKE);
+        fields[5][7] = new Piece(Rank.LAKE);
     }
 
     public void setField(int y, int x, Piece piece){
