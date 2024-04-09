0f5ecf74fcd4ae8b931e0133d258eeb6c02bfc50
= <hans.griesser@gmail.com>
2024-04-09 18:20:10 +0200

    feat: Join Lobby

--- src/main/java/com/example/stratego/connection/broker/WebSocketBrokerConfig.java
+++ src/main/java/com/example/stratego/connection/broker/WebSocketBrokerConfig.java
@@ -1,4 +1,4 @@
-package com.example.stratego.websocket.broker;
+package com.example.stratego.connection.broker;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Configuration;
--- src/main/java/com/example/stratego/connection/broker/WebSocketLobbyController.java
+++ src/main/java/com/example/stratego/connection/broker/WebSocketLobbyController.java
@@ -1,17 +1,23 @@
-package com.example.stratego.websocket.broker;
+package com.example.stratego.connection.broker;
 
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 import session.Player;
 
+import java.util.List;
+import java.util.ArrayList;
+
 @Controller
 public class WebSocketLobbyController {
 
+    private List<Player> waitingPlayers = new ArrayList<>();
+
     @MessageMapping("/join")
     @SendTo("/topic/lobby")
-    public Player joinLobby(Player player) {
-        return player;
+    public List<Player> joinLobby(Player player) {
+        waitingPlayers.add(player);
+        return waitingPlayers;
     }
 }
 
