20f857c20b9a3aea1857cc21ba7589638018d524
= <hans.griesser@gmail.com>
2024-04-10 00:29:45 +0200

    fix: Adjusted import statements because change in package structure

--- src/test/java/SessionServiceTests/BoardTest.java
+++ src/test/java/SessionServiceTests/BoardTest.java
@@ -1,9 +1,9 @@
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import session.Board;
-import session.Piece;
-import session.Rank;
+import com.example.stratego.session.Board;
+import com.example.stratego.session.Piece;
+import com.example.stratego.session.Rank;
 
 import static org.junit.jupiter.api.Assertions.*;
 
