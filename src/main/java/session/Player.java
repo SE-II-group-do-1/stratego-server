c120389191624a95e0319060028b9e94b0d504f1
= <hans.griesser@gmail.com>
2024-04-09 11:36:52 +0200

    feat: Implemented Lobby Prototype

--- src/main/java/session/Player.java
+++ src/main/java/session/Player.java
@@ -1,11 +1,41 @@
 package session;
 
+import java.util.Objects;
+
 public class Player {
     private String username;
     private int id;
 
-    public Player(int id, String username){
+    public Player() {
+        // Default constructor
+    }
+
+    public Player(int id, String username) {
         this.id = id;
         this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Player{" +
+                "username='" + username + '\'' +
+                ", id=" + id +
+                '}';
     }
 }
