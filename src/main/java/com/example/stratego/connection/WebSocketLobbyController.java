4a23935a863b836b28bd1df9d6262729d97810f5
klaus-maria <63562466+klaus-maria@users.noreply.github.com>
2024-06-01 12:48:49 +0200

    fix: log correctly + wrapped endpoints in try-catch, send exception to client

--- src/main/java/com/example/stratego/connection/WebSocketLobbyController.java
+++ src/main/java/com/example/stratego/connection/WebSocketLobbyController.java
@@ -27,43 +27,47 @@ public class WebSocketLobbyController {
 
     @MessageMapping("/join")
     public void joinLobby(String username) {
-        logger.log(Level.INFO, "join endpoint reached. received: {}", username);
-        //check for active sessions
-        //if one in waiting = add to that lobby, else create new with corresponding topic
-        //usernames must be different
-        //return lobby ID, both players (return only when session full)
-        Player player = SessionService.newPlayer(username);
-        Map<String, Object> toReturn = new HashMap<>();
-        List<SessionService> active = SessionService.getActiveSessions();
-        for(SessionService session: active){
-            if(session.getCurrentGameState() == GameState.WAITING && !session.getPlayerBlue().getUsername().equals(player.getUsername())){
-                session.setPlayerRed(player);
-                toReturn.put("id", session.getId());
-                toReturn.put("playerBlueName", session.getPlayerBlue().getUsername());
-                toReturn.put("playerBlueID", session.getPlayerBlue().getId());
-                toReturn.put("playerRedName", player.getUsername());
-                toReturn.put("playerRedID", player.getId());
-                this.template.convertAndSend("/topic/reply", toReturn);
+        logger.log(Level.INFO, "join endpoint reached. received: {0}", username);
+        try {
+            //check for active sessions
+            //if one in waiting = add to that lobby, else create new with corresponding topic
+            //usernames must be different
+            //return lobby ID, both players (return only when session full)
+            Player player = SessionService.newPlayer(username);
+            Map<String, Object> toReturn = new HashMap<>();
+            List<SessionService> active = SessionService.getActiveSessions();
+            for (SessionService session : active) {
+                if (session.getCurrentGameState() == GameState.WAITING && !session.getPlayerBlue().getUsername().equals(player.getUsername())) {
+                    session.setPlayerRed(player);
+                    toReturn.put("id", session.getId());
+                    toReturn.put("playerBlueName", session.getPlayerBlue().getUsername());
+                    toReturn.put("playerBlueID", session.getPlayerBlue().getId());
+                    toReturn.put("playerRedName", player.getUsername());
+                    toReturn.put("playerRedID", player.getId());
+                    this.template.convertAndSend("/topic/reply", toReturn);
+                }
             }
+            new SessionService(player);
+        } catch (Exception e){
+            sendException(e);
         }
-        new SessionService(player);
     }
 
     @MessageMapping("/update")
     public void updateGame(Map<String, Object> message){
-        logger.log(Level.INFO, "update endpoint reached. received: {}", message);
-        int initiator = (int) message.get("initiator");
-        Board board = (Board) message.get("board");
-        int lobbyID = (int) message.get("lobby");
-
-        SessionService session = SessionService.getActiveSessions().stream()
-                .filter( s -> s.getId() == lobbyID)
-                .toList()
-                .get(1);
+        logger.log(Level.INFO, "update endpoint reached. received: {0}", message);
         try {
+            int initiator = (int) message.get("initiator");
+            Board board = (Board) message.get("board");
+            int lobbyID = (int) message.get("lobby");
+
+            SessionService session = SessionService.getActiveSessions().stream()
+                    .filter( s -> s.getId() == lobbyID)
+                    .toList()
+                    .get(1);
             session.updateBoard(board, initiator);
             this.template.convertAndSend("/topic/lobby-"+session.getId(),session.getBoard());
-        } catch (InvalidPlayerTurnException e) {
+        } catch (Exception e) {
             sendException(e);
         }
     }
@@ -72,27 +76,32 @@ public class WebSocketLobbyController {
 
     @MessageMapping("/leave")
     public void leaveLobby(int message){
-        logger.log(Level.INFO, "leave endpoint reached. received: {}", message);        //check if player exists
+        logger.log(Level.INFO, "leave endpoint reached. received: {0}", message);        //check if player exists
         //check if in active lobby -> send to lobby that closed
-        Player player = SessionService.getActivePlayers().stream()
+        try{
+            Player player = SessionService.getActivePlayers().stream()
                 .filter( p -> p.getId() == message)
                 .toList()
                 .get(1);
-        if(SessionService.getActivePlayers().contains(player)){
+            if(SessionService.getActivePlayers().contains(player)) {
 
-            int sessionID = SessionService.getActiveSessions().stream()
-                    .filter( s -> s.getPlayerBlue() == player || s.getPlayerRed() == player)
+                int sessionID = SessionService.getActiveSessions().stream()
+                    .filter(s -> s.getPlayerBlue() == player || s.getPlayerRed() == player)
                     .toList()
                     .get(1)
                     .getId();
 
-            SessionService session = SessionService.getActiveSessions().stream()
-                    .filter( s -> s.getId() == sessionID)
+                SessionService session = SessionService.getActiveSessions().stream()
+                    .filter(s -> s.getId() == sessionID)
                     .toList()
                     .get(1);
 
-            this.template.convertAndSend("/topic/lobby-"+session.getId(), "close");
+            this.template.convertAndSend("/topic/lobby-" + session.getId(), "close");
             session.close();
+            }
+        }
+        catch (Exception e){
+            sendException(e);
         }
     }
 
