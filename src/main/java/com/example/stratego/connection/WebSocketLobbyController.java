52fc415b272f66cbd2300d5ab4b7c003148a46d7
Hans <hans.griesser@gmail.com>
2024-06-11 10:45:36 +0200

    fix: merged conflicts with myself (a short story of learning git features)

--- src/main/java/com/example/stratego/connection/WebSocketLobbyController.java
+++ src/main/java/com/example/stratego/connection/WebSocketLobbyController.java
@@ -17,7 +17,7 @@ import java.util.logging.Level;
 @Controller
 public class WebSocketLobbyController {
 
-    java.util.logging.Logger logger =  java.util.logging.Logger.getLogger(this.getClass().getName());
+    java.util.logging.Logger logger = java.util.logging.Logger.getLogger(this.getClass().getName());
 
     private final SimpMessagingTemplate template;
 
@@ -50,90 +50,66 @@ public class WebSocketLobbyController {
                 }
             }
             new SessionService(player);
-        } catch (Exception e){
+        } catch (Exception e) {
             sendException(e);
         }
     }
 
     @MessageMapping("/update")
-<<<<<<< Updated upstream
-    public void updateGame(Map<String, Object> message){
-        logger.log(Level.INFO, "update endpoint reached. received: {0}", message);
-=======
-    public void updateGame(UpdateMessage updateMessage){
+    public void updateGame(UpdateMessage updateMessage) {
         logger.log(Level.INFO, "update endpoint reached. received: {}", updateMessage);
-        int initiator = updateMessage.getInitiator();
-        Board board = updateMessage.getBoard();
-        int lobbyID = updateMessage.getLobbyID();
-
-        SessionService session = SessionService.getActiveSessions().stream()
-                .filter( s -> s.getId() == lobbyID)
-                .toList()
-                .get(0);
->>>>>>> Stashed changes
         try {
-            logger.log(Level.INFO, "Start of Try Catch Blog (Debugging)");
-            int initiator = (int) message.get("initiator");
-            Piece[][] fields = (Piece[][]) message.get("board");
-            Board board = new Board();
-            board.setBoard(fields);
-            int lobbyID = (int) message.get("lobby");
-
-            logger.log(Level.INFO, "after parsing message. lobbyID: {0}", lobbyID);
+            int initiator = updateMessage.getInitiator();
+            Board board = updateMessage.getBoard();
+            int lobbyID = updateMessage.getLobbyID();
 
             SessionService session = SessionService.getActiveSessions().stream()
-                    .filter( s -> s.getId() == lobbyID)
+                    .filter(s -> s.getId() == lobbyID)
                     .toList()
-                    .get(-10);
+                    .get(0);
+
+
             session.updateBoard(board, initiator);
-<<<<<<< Updated upstream
-            logger.log(Level.INFO, "session id: {0}", session.getId());
-            this.template.convertAndSend("/topic/lobby-"+session.getId(),session.getBoard().getBoard());
-        } catch (Exception e) {
-=======
-            this.template.convertAndSend("/topic/lobby-"+lobbyID,session.getBoard());
+            this.template.convertAndSend("/topic/lobby-" + lobbyID, session.getBoard());
         } catch (InvalidPlayerTurnException e) {
->>>>>>> Stashed changes
             sendException(e);
         }
     }
 
 
-
     @MessageMapping("/leave")
-    public void leaveLobby(int message){
+    public void leaveLobby(int message) {
         logger.log(Level.INFO, "leave endpoint reached. received: {0}", message);        //check if player exists
         //check if in active lobby -> send to lobby that closed
-        try{
+        try {
             Player player = SessionService.getActivePlayers().stream()
-                .filter( p -> p.getId() == message)
-                .toList()
-                .get(1);
-            if(SessionService.getActivePlayers().contains(player)) {
-
-                int sessionID = SessionService.getActiveSessions().stream()
-                    .filter(s -> s.getPlayerBlue() == player || s.getPlayerRed() == player)
-                    .toList()
-                    .get(1)
-                    .getId();
-
-                SessionService session = SessionService.getActiveSessions().stream()
-                    .filter(s -> s.getId() == sessionID)
+                    .filter(p -> p.getId() == message)
                     .toList()
                     .get(1);
+            if (SessionService.getActivePlayers().contains(player)) {
 
-            this.template.convertAndSend("/topic/lobby-" + session.getId(), "close");
-            session.close();
+                int sessionID = SessionService.getActiveSessions().stream()
+                        .filter(s -> s.getPlayerBlue() == player || s.getPlayerRed() == player)
+                        .toList()
+                        .get(1)
+                        .getId();
+
+                SessionService session = SessionService.getActiveSessions().stream()
+                        .filter(s -> s.getId() == sessionID)
+                        .toList()
+                        .get(1);
+
+                this.template.convertAndSend("/topic/lobby-" + session.getId(), "close");
+                session.close();
             }
-        }
-        catch (Exception e){
+        } catch (Exception e) {
             sendException(e);
         }
     }
 
     @MessageExceptionHandler
     @SendToUser("/topic/errors")
-    public String sendException(Throwable exception){
+    public String sendException(Throwable exception) {
         return exception.toString();
     }
 }
