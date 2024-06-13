70e2cbe2159da98d53ca83123f7d2c95d57d8f96
Hans <hans.griesser@gmail.com>
2024-06-13 14:47:00 +0200

    fix(server): correct serialazition naming convention

--- src/main/java/com/example/stratego/session/Piece.java
+++ src/main/java/com/example/stratego/session/Piece.java
@@ -7,16 +7,18 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 public class Piece {
     private Rank rank;
 
-    private boolean isVisible;
-    private boolean isMovable;
+    private boolean visible;
+
+    private boolean movable;
 
     private Color color;
 
-    public Piece(Rank rank, Color color){
+    @JsonCreator
+    public Piece(@JsonProperty("rank") Rank rank, @JsonProperty("color") Color color){
         this.color = color;
         this.rank = rank;
-        this.isVisible = false;
-        this.isMovable = rank != Rank.LAKE && rank != Rank.FLAG && rank != Rank.BOMB;
+        this.visible = false;
+        this.movable = rank != Rank.LAKE && rank != Rank.FLAG && rank != Rank.BOMB;
     }
 
     /**
@@ -27,20 +29,9 @@ public class Piece {
     public Piece(Rank rank) throws WrongConstructorException {
         if(rank != Rank.LAKE) throw new WrongConstructorException();
         this.rank = rank;
-        this.isVisible = true;
-        this.isMovable = false;
+        this.visible = true;
+        this.movable = false;
         this.color = null;
-    }
-
-    @JsonCreator
-    public Piece(@JsonProperty("color") Color color,
-                 @JsonProperty("isMovable") boolean isMovable,
-                 @JsonProperty("isVisible") boolean isVisible,
-                 @JsonProperty("rank") Rank rank) {
-        this.color = color;
-        this.isMovable = isMovable;
-        this.isVisible = isVisible;
-        this.rank = rank;
     }
 
     public Rank getRank() {
@@ -48,14 +39,14 @@ public class Piece {
     }
 
     public boolean isVisible() {
-        return isVisible;
+        return visible;
     }
 
     public void setVisible(boolean visible) {
-        isVisible = visible;
+        this.visible = visible;
     }
     public boolean isMovable() {
-        return isMovable;
+        return movable;
     }
 
     public Color getColor() {
