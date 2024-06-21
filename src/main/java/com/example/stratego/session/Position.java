652ea53c4be3b1ecbf03016f22a5a4c9a59ad743
Hans <hans.griesser@gmail.com>
2024-06-21 18:30:55 +0200

    fix: json stuff

--- src/main/java/com/example/stratego/session/Position.java
+++ src/main/java/com/example/stratego/session/Position.java
@@ -1,13 +1,14 @@
 package com.example.stratego.session;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class Position {
     int x;
     int y;
 
     @JsonCreator
-    public Position(int x, int y) {
+    public Position(@JsonProperty("x") int x,@JsonProperty("y") int y) {
         this.x = x;
         this.y = y;
     }
